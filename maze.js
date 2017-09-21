47dfd6e1ff119befc6a116c32cf0f7838dc70374
Francisco Machado <franm912@gmail.com>
2017-09-21 02:29:51 +0100

    Refactor maze constructor

--- maze.js
+++ maze.js
@@ -65,33 +65,80 @@ function Maze(w, h) {
 	this.startPoint.visited = true;
 	this.currentCell = this.startPoint;
 
-	// Initialize stack
+	// Initialize stack used to create and solve maze
 	this.stack = [this.currentCell];
 	this.maxStack = 0;
 
 	this.move = function() {
-		var next = this.next();
+		var direction = this.getUnvisitedCellDirection();
 
-		if(next) {
-			next.visited = true;
-			next.html.className += ' highlight';
-			this.stack.push(next);
-			this.currentCell = next;
+		if(direction) {
+			switch(direction){
+				case 'top':
+						this.currentCell.top = true;
+						this.currentCell.html.className += ' top';
+
+						this.currentCell = this.cells[this.currentCell.y - 1][this.currentCell.x];
+
+						this.currentCell.bottom = true;
+						this.currentCell.html.className += ' bottom';
+					break;
+
+				case 'right':
+						this.currentCell.right = true;
+						this.currentCell.html.className += ' right';
+
+						this.currentCell = this.cells[this.currentCell.y][this.currentCell.x + 1];
+
+						this.currentCell.left = true;
+						this.currentCell.html.className += ' left';
+					break;
+
+				case 'bottom':
+						this.currentCell.bottom = true;
+						this.currentCell.html.className += ' bottom';
+
+						this.currentCell = this.cells[this.currentCell.y + 1][this.currentCell.x];
+
+						this.currentCell.top = true;
+						this.currentCell.html.className += ' top';
+					break;
+
+				case 'left':
+						this.currentCell.left = true;
+						this.currentCell.html.className += ' left';
+
+						this.currentCell = this.cells[this.currentCell.y][this.currentCell.x - 1];
+
+						this.currentCell.right = true;
+						this.currentCell.html.className += ' right';
+					break;
+			}
+
+			this.currentCell.visited = true;
+			this.currentCell.html.className += ' highlight';
+			this.stack.push(this.currentCell);
 		}
 		else{
+			// Check if new path distance is bigger than the previous biggest
 			if(this.maxStack < this.stack.length){
 				this.endPoint = this.currentCell;
 				this.maxStack = this.stack.length;
 			}
 
+			// Remove highlight
 			this.currentCell.html.className = this.currentCell.html.className.replace(' highlight', '');
+
+			// Set new current place
 			this.stack.pop();
 			this.currentCell = this.stack[this.stack.length - 1]
 
+			// Check if all cells are visited
 			if (this.stack.length == 1) {
 				this.currentCell.html.className = this.currentCell.html.className.replace(' highlight', '');
 
 				this.endPoint.html.className += ' end';
+				this.resetCells();
 
 				return false;
 			}
@@ -101,13 +148,30 @@ function Maze(w, h) {
 	}
 
 	this.solve = function() {
-		var next = this.makePath();
+		var direction = this.getUnvisitedCellDirection();
 
-		if(next) {
-			next.visited = false;
-			next.html.className += ' path';
-			this.stack.push(next);
-			this.currentCell = next;
+		if(direction) {
+			switch(direction){
+				case 'top':
+						this.currentCell = this.cells[this.currentCell.y - 1][this.currentCell.x];
+					break;
+
+				case 'right':
+						this.currentCell = this.cells[this.currentCell.y][this.currentCell.x + 1];
+					break;
+
+				case 'bottom':
+						this.currentCell = this.cells[this.currentCell.y + 1][this.currentCell.x];
+					break;
+
+				case 'left':
+						this.currentCell = this.cells[this.currentCell.y][this.currentCell.x - 1];
+					break;
+			}
+
+			this.currentCell.visited = true;
+			this.currentCell.html.className += ' path';
+			this.stack.push(this.currentCell);
 
 			if (this.currentCell == this.endPoint)
 				return true;
@@ -119,23 +183,9 @@ function Maze(w, h) {
 		}
 	}
 
-	this.makePath = function() {
-		if (this.currentCell.top && this.cells[this.currentCell.y - 1][this.currentCell.x].visited)
-			return this.cells[this.currentCell.y - 1][this.currentCell.x];
-
-		if (this.currentCell.right && this.cells[this.currentCell.y][this.currentCell.x + 1].visited)
-			return this.cells[this.currentCell.y][this.currentCell.x + 1];
-
-		if (this.currentCell.bottom && this.cells[this.currentCell.y + 1][this.currentCell.x].visited)
-			return this.cells[this.currentCell.y + 1][this.currentCell.x];
-
-		if (this.currentCell.left && this.cells[this.currentCell.y][this.currentCell.x - 1].visited)
-			return this.cells[this.currentCell.y][this.currentCell.x - 1];
-
-		return false;
-	}
-
-	this.next = function() {
+	// Returns a random direction with an unvisited cell.
+	// Returns false if all cells around currentCell were already visited.
+	this.getUnvisitedCellDirection = function() {
 		var rand = ['top', 'right', 'bottom', 'left'];
 
 		while(rand.length > 0) {
@@ -143,66 +193,87 @@ function Maze(w, h) {
 
 			switch(rand[n]) {
 				case 'top':
-					if(this.currentCell.y - 1 >= 0 &&
-					this.cells[this.currentCell.y - 1][this.currentCell.x].visited === false) {
-						var aux = this.cells[this.currentCell.y - 1][this.currentCell.x];
+						// Check if there's an unvisited cell above currentCell
+						if(this.currentCell.y - 1 >= 0 &&
+						this.cells[this.currentCell.y - 1][this.currentCell.x].visited === false) {
+							return 'top';
+							var aux = this.cells[this.currentCell.y - 1][this.currentCell.x];
 
-						aux.bottom = true;
-						aux.html.className += ' bottom';
+							aux.bottom = true;
+							aux.html.className += ' bottom';
 
-						this.currentCell.top = true;
-						this.currentCell.html.className += ' top';
+							this.currentCell.top = true;
+							this.currentCell.html.className += ' top';
 
-						return aux; // Return next this.currentCell
-					}
+							return aux; // Return next this.currentCell
+						}
 					break;
+
 				case 'right':
-					if(this.currentCell.x + 1 < width &&
-					this.cells[this.currentCell.y][this.currentCell.x + 1].visited === false) {
-						var aux = this.cells[this.currentCell.y][this.currentCell.x + 1];
+						// Check if there's an unvisited cell to the right of currentCell
+						if(this.currentCell.x + 1 < width &&
+						this.cells[this.currentCell.y][this.currentCell.x + 1].visited === false) {
+							return 'right';
+							var aux = this.cells[this.currentCell.y][this.currentCell.x + 1];
 
-						this.currentCell.right = true;
-						this.currentCell.html.className += ' right';
+							this.currentCell.right = true;
+							this.currentCell.html.className += ' right';
 
-						aux.left = true;
-						aux.html.className += ' left'
+							aux.left = true;
+							aux.html.className += ' left'
 
-						return aux;
-					}
+							return aux;
+						}
 					break;
+
 				case 'bottom':
-					if(this.currentCell.y + 1 < height &&
-					this.cells[this.currentCell.y + 1][this.currentCell.x].visited === false) {
-						var aux = this.cells[this.currentCell.y + 1][this.currentCell.x];
+						// Check if there's an unvisited cell below currentCell
+						if(this.currentCell.y + 1 < height &&
+						this.cells[this.currentCell.y + 1][this.currentCell.x].visited === false) {
+							return 'bottom';
+							var aux = this.cells[this.currentCell.y + 1][this.currentCell.x];
 
-						this.currentCell.bottom = true;
-						this.currentCell.html.className += ' bottom';
+							this.currentCell.bottom = true;
+							this.currentCell.html.className += ' bottom';
 
-						aux.top = true;
-						aux.html.className += ' top';
+							aux.top = true;
+							aux.html.className += ' top';
 
-						return aux;
-					}
+							return aux;
+						}
 					break;
+
 				case 'left':
-					if(this.currentCell.x - 1 >= 0 && this.cells[this.currentCell.y][this.currentCell.x - 1].visited === false) {
-						var aux = this.cells[this.currentCell.y][this.currentCell.x - 1];
+						// Check if there's an unvisited cell left of currentCell
+						if(this.currentCell.x - 1 >= 0 &&
+						this.cells[this.currentCell.y][this.currentCell.x - 1].visited === false) {
+							return 'left';
+							var aux = this.cells[this.currentCell.y][this.currentCell.x - 1];
 
-						aux.right = true;
-						aux.html.className += ' right';
+							aux.right = true;
+							aux.html.className += ' right';
 
-						this.currentCell.left = true;
-						this.currentCell.html.className += ' left';
+							this.currentCell.left = true;
+							this.currentCell.html.className += ' left';
 
-						return aux;
-					}
+							return aux;
+						}
 					break;
 			}
 
 			rand.splice(n,1); // Remove direction if cell is already visited
 		}
 
-		return false; // In case all around are visited
+		return false; // In case all cells around are visited
+	}
+
+	// Resets all cells to unvisited
+	this.resetCells = function() {
+		for(var i = 0; i < this.cells.length; i++){
+			for(var j = 0; j < this.cells[i].length; j++){
+				this.cells[i][j].visited = false;
+			}
+		}
 	}
 }
 
